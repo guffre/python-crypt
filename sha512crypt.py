eff343524a93abf9fbba94b77333ebb588f1623f
guffre <mtguffr@gmail.com>
2023-06-17 02:38:13 -0500

    Made sha512crypt.py python 2 and 3 compatable

--- sha512crypt.py
+++ sha512crypt.py
@@ -1,97 +1,20 @@
 import hashlib
-import binascii
 import base64
-import sys
+import string
 
-# Testing with a password >64 characters
-key = sys.argv[1].encode()
-salt = sys.argv[2].encode()
-rounds = int(sys.argv[3])
-
-ctx = key + salt
-alt_ctx = key + salt + key
-
-# Testing with both "hash.update" and just appending the string
-# Not sure which I will ultimately decide to use
-result = hashlib.sha512(ctx)
-alt_result = hashlib.sha512(alt_ctx).digest()
-#print("alt result: ", hashlib.sha512(alt_ctx).hexdigest())
-
-# All below code is mimic-ing the C source.
-# I will update this with more "pythonic" code when I finish translating all the C
-cnt = len(key)
-while cnt > 64:
-    ctx += alt_result[:64]
-    result.update(alt_result[:64])
-    cnt -= 64
-
-ctx += alt_result[:cnt]
-result.update(alt_result[:cnt])
-
-cnt = len(key)
-while cnt > 0:
-    if (cnt & 1) != 0:
-        ctx += alt_result[:64]
-        result.update(alt_result[:64])
-    else:
-        ctx += key
-        result.update(key)
-    cnt = cnt >> 1
-
-# INTERMEDIATE RESULTS
-alt_result = hashlib.sha512(ctx).digest()
-#print(hashlib.sha512(ctx).hexdigest())
-
-alt_ctx = b""
-cnt = 0
-while (cnt < len(key)):
-    alt_ctx += key
-    cnt += 1
-
-temp_result = hashlib.sha512(alt_ctx)
-#print(temp_result.hexdigest())
-temp_result = temp_result.digest()
-
-p_bytes = b""
-for n in range(len(key)):
-    p_bytes += temp_result[n%len(temp_result):n+1]
-#p_bytes = b"".join([temp_result[n%len(temp_result)] for n in range(len(key))])
-
-
-alt_ctx = b""
-cnt = 0
-while (cnt < 16 + alt_result[0]):
-    alt_ctx += salt
-    cnt += 1
-
-temp_result = hashlib.sha512(alt_ctx).digest()
-#print(binascii.hexlify(temp_result))
-#s_bytes = b"".join([temp_result[n%len(temp_result)] for n in range(len(salt))])
-s_bytes = b""
-for n in range(len(salt)):
-    s_bytes += temp_result[n%len(temp_result):n+1]
-
-cnt = 0
-while cnt < rounds:
-    ctx = b""
-    if ((cnt & 1) != 0):
-        ctx += p_bytes[:len(key)]
-    else:
-        ctx += alt_result[:64]
-    if (cnt % 3 != 0):
-        ctx += s_bytes[:len(salt)]
-    if (cnt % 7 != 0):
-        ctx += p_bytes[:len(key)]
-    if ((cnt & 1) != 0):
-        ctx += alt_result[:64]
-    else:
-        ctx += p_bytes[:len(key)]
-    alt_result = hashlib.sha512(ctx).digest()
-    cnt += 1
-
-print("Final hash:", binascii.hexlify(alt_result))
+def make_bytes(data, length):
+    """ Used to make context data for the "rounds" of sha hashing"""
+    alt_ctx = hashlib.sha512()
+    for n in range(length):
+        alt_ctx.update(data)
+    temp_result = alt_ctx.digest()
+        
+    tmp_bytes = b"".join(temp_result[n%len(temp_result):n+1] for n in range(length))
+    return tmp_bytes
 
 def crypt_base64(buffer):
+    """The custom base64 that is specific to these crypt algorithms
+       I know it looks weird, but this is apparently what the spec is"""
     unix_crypt_base = b'./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     normal_base     = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     ret = b""
@@ -103,8 +26,67 @@ def crypt_base64(buffer):
             collect += buffer[m:m+1]
         ret += base64.b64encode(collect)[::-1]
     ret += base64.b64encode(b"00"+buffer[63:64])[::-1][:2]
-    ret = ret.translate(bytes.maketrans(normal_base,unix_crypt_base))
+    # Python 2/3 compatability hack
+    trans = string if bytes == str else bytes
+    ret = ret.translate(trans.maketrans(normal_base,unix_crypt_base))
     return ret
 
-encoded = crypt_base64(alt_result)
-print("Encoded hash: ", encoded)
+def crypt_sha512(key, salt, rounds=5000):
+    # Initialize the two contexts
+    ctx        = hashlib.sha512(key + salt)
+    alt_ctx    = hashlib.sha512(key + salt + key)
+    alt_result = alt_ctx.digest()
+
+    # Add hash-bytes of second context to first
+    cnt = len(key)
+    while cnt > ctx.digest_size:
+        ctx.update(alt_result)
+        cnt -= ctx.digest_size
+    ctx.update(alt_result[:cnt])
+
+    # Iterates through the binary representation of the key
+    # If binary 1, add the "alternate sum"; if binary 0, add the key
+    cnt = len(key)
+    while cnt > 0:
+        if (cnt & 1) != 0:
+            ctx.update(alt_result)
+        else:
+            ctx.update(key)
+        cnt = cnt >> 1
+
+    # Create byte arrays. These will be used in the rounds of hashing
+    alt_result = ctx.digest()
+    if isinstance(alt_result, str):
+        alt_result = bytearray(alt_result)
+    p_bytes = make_bytes(key, len(key))
+    s_bytes = make_bytes(salt, 16 + alt_result[0])[:len(salt)]
+
+    # Perform rounds of hashing
+    for cnt in range(rounds):
+        ctx = hashlib.sha512()
+        if ((cnt & 1) != 0):
+            ctx.update(p_bytes)
+        else:
+            ctx.update(alt_result)
+        if (cnt % 3 != 0):
+            ctx.update(s_bytes)
+        if (cnt % 7 != 0):
+            ctx.update(p_bytes)
+        if ((cnt & 1) != 0):
+            ctx.update(alt_result)
+        else:
+            ctx.update(p_bytes)
+        alt_result = ctx.digest()
+
+    encoded = crypt_base64(alt_result)
+    return (alt_result,encoded)
+
+if __name__ == '__main__':
+    import binascii
+    import sys
+    try:
+        digest,hash = crypt_sha512(sys.argv[1].encode(), sys.argv[2].encode(), int(sys.argv[3]))
+        print("Digest: {}".format(binascii.hexlify(digest)))
+        print("Hash: {}".format(hash))
+    except:
+        print("Usage: ./{} <password> <hash> <rounds>".format(sys.argv[0]))
