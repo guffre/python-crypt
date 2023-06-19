efb56a37337b373a1b25aa3aff027688855b2620
guffre <mtguffr@gmail.com>
2023-06-19 18:34:30 -0500

    Support sha256-crypt and sha512-crypt

--- sha512crypt.py
+++ sha512crypt.py
@@ -2,14 +2,14 @@ import hashlib
 import base64
 import string
 
-def make_bytes(data, length):
-    """ Used to make context data for the "rounds" of sha hashing"""
-    alt_ctx = hashlib.sha512()
-    for n in range(length):
+def make_bytes(data, rounds, hash):
+    """ Used to make p_bytes and s_bytes for the sha hashing rounds"""
+    alt_ctx = hash()
+    for n in range(rounds):
         alt_ctx.update(data)
     temp_result = alt_ctx.digest()
         
-    tmp_bytes = b"".join(temp_result[n%len(temp_result):n+1] for n in range(length))
+    tmp_bytes = b"".join(temp_result[n%len(temp_result):n+1] for n in range(len(data)))
     return tmp_bytes
 
 def crypt_base64(buffer):
@@ -18,23 +18,34 @@ def crypt_base64(buffer):
     unix_crypt_base = b'./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     normal_base     = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     ret = b""
-    for n in range(0,22*21,22):
-        n = n%63
-        collect = b""
-        for m in range(n,n+21*3,21):
-            m = m%63
-            collect += buffer[m:m+1]
+    if len(buffer) == 32:
+        c1 = [(n*21+00)%30 for n in range(10)]
+        c2 = [(n*21+10)%30 for n in range(10)]
+        c3 = [(n*21+20)%30 for n in range(10)]
+    elif len(buffer) == 64:
+        c1 = [(n*22+00)%63 for n in range(21)]
+        c2 = [(n*22+21)%63 for n in range(21)]
+        c3 = [(n*22+42)%63 for n in range(21)]
+    else:
+        return None
+    for block in zip(c1, c2, c3):
+        collect = b''.join(buffer[n:n+1] for n in block)
         ret += base64.b64encode(collect)[::-1]
-    ret += base64.b64encode(b"00"+buffer[63:64])[::-1][:2]
-    # Python 2/3 compatability hack
+    if len(buffer) == 32:
+        ret += base64.b64encode(b"0"+buffer[31:32]+buffer[30:31])[::-1][:2]
+    elif len(buffer) == 64:
+        ret += base64.b64encode(b"00"+buffer[63:64])[::-1][:2]
+    # Python 2/3 compatability
     trans = string if bytes == str else bytes
     ret = ret.translate(trans.maketrans(normal_base,unix_crypt_base))
     return ret
 
-def crypt_sha512(key, salt, rounds=5000):
+def crypt_sha512(key, salt, rounds=5000, hash=hashlib.sha512):
+    """ sha512-crypt and sha256-crypt both have the exact same pattern
+        This function allows you to do both, but defaults to sha512-crypt"""
     # Initialize the two contexts
-    ctx        = hashlib.sha512(key + salt)
-    alt_ctx    = hashlib.sha512(key + salt + key)
+    ctx        = hash(key + salt)
+    alt_ctx    = hash(key + salt + key)
     alt_result = alt_ctx.digest()
 
     # Add hash-bytes of second context to first
@@ -56,14 +67,13 @@ def crypt_sha512(key, salt, rounds=5000):
 
     # Create byte arrays. These will be used in the rounds of hashing
     alt_result = ctx.digest()
-    if isinstance(alt_result, str):
-        alt_result = bytearray(alt_result)
-    p_bytes = make_bytes(key, len(key))
-    s_bytes = make_bytes(salt, 16 + alt_result[0])[:len(salt)]
+    offset = ord(alt_result[0]) if isinstance(alt_result, str) else alt_result[0]
+    p_bytes = make_bytes(key, len(key), hash)
+    s_bytes = make_bytes(salt, 16 + offset, hash)
 
     # Perform rounds of hashing
     for cnt in range(rounds):
-        ctx = hashlib.sha512()
+        ctx = hash()
         if ((cnt & 1) != 0):
             ctx.update(p_bytes)
         else:
@@ -78,15 +88,33 @@ def crypt_sha512(key, salt, rounds=5000):
             ctx.update(p_bytes)
         alt_result = ctx.digest()
 
-    encoded = crypt_base64(alt_result)
-    return (alt_result,encoded)
+    # Crypt-base64 the hash digest
+    encoded = crypt_base64(alt_result).decode()
+
+    # Make some nice output
+    hashnumber = 5 if hash == hashlib.sha256 else 6
+    if rounds == 5000:
+        formatted = "${}${}${}".format(hashnumber,salt.decode(),encoded)
+    else:
+        rounds = "rounds={}".format(rounds)
+        formatted = "${}${}${}${}".format(hashnumber,rounds,salt.decode(),encoded)
+    return (alt_result,formatted)
 
 if __name__ == '__main__':
     import binascii
     import sys
     try:
-        digest,hash = crypt_sha512(sys.argv[1].encode(), sys.argv[2].encode(), int(sys.argv[3]))
-        print("Digest: {}".format(binascii.hexlify(digest)))
-        print("Hash: {}".format(hash))
+        key  = sys.argv[1].encode()
+        salt = sys.argv[2].encode()
+        if len(sys.argv) == 4:
+            rounds = int(sys.argv[3])
+        else:
+            rounds = 5000
+        digest,hash = crypt_sha512(key, salt, rounds)
+        print("sha512-crypt digest: {}".format(binascii.hexlify(digest)))
+        print("sha512-crypt hash: {}".format(hash))
+        digest,hash = crypt_sha512(key, salt, rounds, hashlib.sha256)
+        print("sha256-crypt digest: {}".format(binascii.hexlify(digest)))
+        print("sha256-crypt hash: {}".format(hash))
     except:
         print("Usage: ./{} <password> <hash> <rounds>".format(sys.argv[0]))
